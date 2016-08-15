667a4214b363b282c5cac84d47bcebed07bbe3b0
simplyred4ever <simplyred4ever@hotmail.com>
2016-08-15 14:06:32 +0800

    add socket.io

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-
 /**
  * Module dependencies.
  */
 var express = require('express'); // 引用express框架
 var partials = require('express-partials'); // Express3.0中引用layout.ejs时
 var routes = require('./routes'); // 路由控制，找routes.js或routes/index.js
-var http = require('http');
 var path = require('path'); // 路径模块
 var favicon = require('serve-favicon');
 var app = express(); // 使用框架
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session); // 连mongo数据库*/
 var settings = require('./settings'); // 引用settings.js*/
@@ -21,6 +21,7 @@ var cookieParser = require('cookie-parser');
 var csrf = require('csurf');
 var errorHandler = require('errorHandler');
 var bodyParser = require('body-parser');
+
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views'); // 模板位置 MVC的V
 app.set('view engine', 'ejs'); // 模板引擎
@@ -28,61 +29,77 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(morgan('dev'));
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({
-		extended : false
-	})); // for parsing application/x-www-form-urlencoded
+    extended: false
+})); // for parsing application/x-www-form-urlencoded
 app.use(cookieParser()); // Cookie解析中间件
 app.use(csrf({
-		cookie : true
-	}));
+    cookie: true
+}));
 app.use(methodOverride('_method')); // override with POST having ?_method=DELETE
 // error handler
-app.use(function (err, req, res, next) {
-	if (err.code !== 'EBADCSRFTOKEN')
-		return next(err);
-	// handle CSRF token errors here
-	res.status(403);
-	res.send('form tampered with');
-})
+app.use(function(err, req, res, next) {
+    if (err.code !== 'EBADCSRFTOKEN') {
+        return next(err);
+    }
+
+    // handle CSRF token errors here
+    res.status(403);
+    res.send('form tampered with');
+});
 app.use(partials()); // Express3.0中引用layout.ejs时
 app.use(flash()); // 闪存工具，通过它保存的变量只会在用户当前和下一次的请求中被访问，之后会被清除*/
 app.use(session({
-		resave : false, //添加这行
-		saveUninitialized : true, //添加这行
-		secret : settings.COOKIE_SECRET,
-		key : settings.db, //cookie name
-		cookie : {
-			maxAge : 1000 * 60 * 60 * 24 * 30
-		}, //30 days
-		store : new MongoStore({
-			username : settings.USERNAME,
-			password : settings.PASSWORD,
-			url : settings.URL,
-			db : settings.DB
-		})
-	}));
+    resave: false, //添加这行
+    saveUninitialized: true, //添加这行
+    secret: settings.COOKIE_SECRET,
+    key: settings.db, //cookie name
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 30
+    }, //30 days
+    store: new MongoStore({
+        username: settings.USERNAME,
+        password: settings.PASSWORD,
+        url: settings.URL,
+        db: settings.DB
+    })
+}));
+app.use(function(req, res, next) {
+    res.locals.user = req.session.user;
+    res.locals.post = req.session.post;
 
-app.use(function (req, res, next) {
-	res.locals.user = req.session.user;
-	res.locals.post = req.session.post;
-	var error = req.flash('error');
-	res.locals.error = error.length ? error : null;
+    var error = req.flash('error');
 
-	var success = req.flash('success');
-	res.locals.success = success.length ? success : null;
-	res.locals._csrf = req.session._csrf; // 防止csrf攻击
-	next();
+    res.locals.error = error.length ? error : null;
+
+    var success = req.flash('success');
+
+    res.locals.success = success.length ? success : null;
+    res.locals._csrf = req.session._csrf; // 防止csrf攻击
+    next();
 });
-app.use(compression({level: 9})); // 压缩中间件 level压缩程度最高
+app.use(compression({
+    level: 9
+})); // 压缩中间件 level压缩程度最高
 app.use(express.static(path.join(__dirname, 'public'))); //设置静态文件目录
+app.io = io;
 routes(app); // 路由控制MVC的C
-
-app.on('close', function () {
-	mongodb.close();
+app.on('close', function() {
+    mongodb.close();
 });
+app.use(errorHandler({
+    // 错误处理
+    dumpExceptions: true,
+    showStack: true
+}));
 
-app.use(errorHandler({ // 错误处理
-		dumpExceptions : true,
-		showStack : true
-	}));
+var users = {};
+var usernames = {};
 
+io.on('connection', function(socket) {
+    console.log("Connection " + socket.id + " accepted.");
+    socket.on('disconnect', function() {
+        console.log("Connection " + socket.id + " terminated.");
+    });
+});
 module.exports = app;
+module.exports.http = http;
--- routes/post.js
+++ routes/post.js
@@ -1,26 +1,27 @@
-
 /*
  * 发言
  */
 var Post = require('../models/post.js');
 var routesUtil = require('./routesUtil.js');
 
-module.exports = function (app) {
-	
-	app.route('/post')
-	.all(routesUtil.checkLogin)
-	.post(function (req, res) {
-		var currentUser = req.session.user;
-		var post = new Post(currentUser.name, req.body.post);
-		post.save(function (err) {
-			if (err) {
-				req.flash('error', err);
-				return res.redirect('/');
-			}
-			req.flash('success', '发表成功');
-			res.redirect('/u/' + currentUser.name);
-		});
-	});
+module.exports = function(app) {
+    app.route('/post').all(routesUtil.checkLogin).post(function(req, res) {
+        var currentUser = req.session.user;
+        var post = new Post(currentUser.name, req.body.post);
+
+        post.save(function(err) {
+            if (err) {
+                req.flash('error', err);
+
+                return res.redirect('/');
+            }
+
+            req.flash('success', '发表成功');
+            res.redirect('/u/' + currentUser.name);
+            app.io.sockets.emit('message', {
+                msg: 'reload',
+                exclude: currentUser.name
+            });
+        });
+    });
 };
-
-

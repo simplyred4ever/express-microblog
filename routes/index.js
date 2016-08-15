1f5aa7635c6adc4648dc9c2cecf367e410f00fc6
simplyred4ever <simplyred4ever@hotmail.com>
2016-08-15 16:28:37 +0800

    format

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,25 @@
-
 /*
  * GET home page.
  */
 var Post = require('../models/post.js');
-module.exports = function (app) {
 
+module.exports = function(app) {
     var routes = ['./reg', './login', './logout', './post', './user', './file'];
+
     for (var i in routes) {
         require(routes[i])(app);
     }
-
     // 首页
-    app.get('/', function (req, res) {
-        Post.get(null, function (err, posts) {
+    app.get('/', function(req, res) {
+        Post.get(null, function(err, posts) {
             if (err) {
                 posts = [];
             }
+
             res.render('index', {
-                title : '首页',
-				csrfToken : req.csrfToken(),
-                posts : posts
+                title: '首页',
+                csrfToken: req.csrfToken(),
+                posts: posts
             });
         });
     });
